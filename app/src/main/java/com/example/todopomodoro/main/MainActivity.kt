1085b610aa4313662917f46271a862fd31944eec
Maciej Najbar <maciek.najbar@gmail.com>
2024-02-20 15:24:50 +0100

    Refactoring - extracting widget

--- app/src/main/java/com/example/todopomodoro/main/MainActivity.kt
+++ app/src/main/java/com/example/todopomodoro/main/MainActivity.kt
@@ -6,20 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.PlayArrow
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,10 +22,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.todopomodoro.main.widgets.NewItemField
 import com.example.todopomodoro.ui.theme.ToDoPomodoroTheme
 
 class MainActivity : ComponentActivity() {
@@ -68,22 +63,6 @@ private fun Screen(
 
         NewItemField(onDoneClicked)
     }
-}
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-private fun NewItemField(onDoneClicked: (String) -> Unit) {
-    var value by remember { mutableStateOf("Finish Android course") }
-    TextField(
-        value = value,
-        onValueChange = { value = it },
-        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-        keyboardActions = KeyboardActions(onDone = {
-            onDoneClicked(value)
-            value = ""
-        }),
-        modifier = Modifier.fillMaxWidth()
-    )
 }
 
 @Composable
---
+++ app/src/main/java/com/example/todopomodoro/main/widgets/NewItemField.kt
@@ -0,0 +1,37 @@
+package com.example.todopomodoro.main.widgets
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun NewItemField(onDoneClicked: (String) -> Unit) {
+    var value by remember { mutableStateOf("Finish Android course") }
+    TextField(
+        value = value,
+        onValueChange = { value = it },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = {
+            onDoneClicked(value)
+            value = ""
+        }),
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+@Preview
+fun NewItemFieldPreview() {
+    NewItemField(onDoneClicked = {})
+}
