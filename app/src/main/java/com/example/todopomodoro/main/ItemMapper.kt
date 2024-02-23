b9c8054a7bb410e8a99166dc7b275d3de5b7d323
Maciej Najbar <maciek.najbar@gmail.com>
2024-02-23 11:23:31 +0100

    Refactoring - date formmatter

--- app/src/main/java/com/example/todopomodoro/main/ItemMapper.kt
+++ app/src/main/java/com/example/todopomodoro/main/ItemMapper.kt
@@ -3,9 +3,10 @@ package com.example.todopomodoro.main
 import androidx.compose.ui.graphics.Color
 import com.example.todopomodoro.domain.ItemEntity
 import java.text.SimpleDateFormat
+import java.util.Locale
 
 class ItemMapper(
-    val dateFormatter: (Long?) -> String = { SimpleDateFormat("dd/MM/yy").format(it) },
+    private val dateFormatter: (Long?) -> String = SimpleDateFormat("dd/MM/yy", Locale.getDefault())::format,
 ) {
     fun map(input: ItemEntity): MainViewModel.ItemModel {
         return MainViewModel.ItemModel(
