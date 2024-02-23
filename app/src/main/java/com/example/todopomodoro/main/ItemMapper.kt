7944cdeb8d6750a2a68bf88d364350bb1497998c
Maciej Najbar <maciek.najbar@gmail.com>
2024-02-23 12:38:58 +0100

    Bug fix - date skips hour minutes and seconds

--- app/src/main/java/com/example/todopomodoro/main/ItemMapper.kt
+++ app/src/main/java/com/example/todopomodoro/main/ItemMapper.kt
@@ -3,23 +3,37 @@ package com.example.todopomodoro.main
 import androidx.compose.ui.graphics.Color
 import com.example.todopomodoro.domain.ItemEntity
 import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
 import java.util.Locale
 
 class ItemMapper(
-    private val dateFormatter: (Long?) -> String =
-        SimpleDateFormat("dd/MM/yy", Locale.getDefault())::format,
+    private val dateFormatter: (Long) -> String =
+        { SimpleDateFormat("dd/MM/yy", Locale.getDefault()).format(Date(it)) },
     private val timestampProvider: () -> Long =
-        { System.currentTimeMillis() },
+        {
+            Calendar.getInstance().apply {
+                time = Date(System.currentTimeMillis())
+                set(Calendar.HOUR, 0)
+                set(Calendar.MINUTE, 0)
+                set(Calendar.SECOND, 0)
+            }.timeInMillis
+        },
 ) {
     fun map(input: ItemEntity): MainViewModel.ItemModel {
-        val now = timestampProvider()
+        val today = timestampProvider()
+        val dateText = if (input.dueDate == null) {
+            "No Date"
+        } else {
+            dateFormatter(input.dueDate)
+        }
         return MainViewModel.ItemModel(
             id = input.id,
             name = input.text,
             isChecked = input.isComplete,
             shouldShowDatePicker = false,
-            dateText = dateFormatter(input.dueDate),
-            dateColor = if (input.dueDate != null && input.dueDate >= now || input.dueDate == null) {
+            dateText = dateText,
+            dateColor = if (input.dueDate == null || input.dueDate >= today) {
                 Color.Black
             } else {
                 Color.Red
